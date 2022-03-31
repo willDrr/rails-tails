5fd24db41ba30592bfb34400d4bcd08f9a1658d8
willDrr <willxdd97@gmail.com>
2022-03-30 18:55:33 -0600

    Adding styles to sign up page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def navigation_items
+    [
+      {body: 'Some route', href: 'some href' },
+      {body: 'Some route', href: 'some href' },
+      {body: 'Some route', href: 'some href' },
+    ]
+  end
+
+  def navigation_class(path)
+    if current_page?(path)
+      "bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+    else
+      "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+    end
+  end
+
+  def navigation_aria(path)
+    if current_page?(path)
+      "page"
+    else
+      "false"
+    end
+  end
 end
