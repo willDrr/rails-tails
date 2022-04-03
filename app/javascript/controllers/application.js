188900ac73941c990aa2560005fe3ea9fa369567
willDrr <willxdd97@gmail.com>
2022-04-02 21:45:36 -0600

    Adding tailwind components stimulus

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,15 @@
 import { Application } from "@hotwired/stimulus"
+import {Popover, Alert} from "tailwindcss-stimulus-components"
+
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+application.register('popover', Popover)
+application.register('alert', Alert)
+
 
 export { application }
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,7 @@
 pin "el-transition", to: "https://ga.jspm.io/npm:el-transition@0.0.7/index.js", preload: true
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tailwindcss-stimulus-components", to: "https://ga.jspm.io/npm:tailwindcss-stimulus-components@3.0.4/dist/tailwindcss-stimulus-components.modern.js"
