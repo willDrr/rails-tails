2121621096dd0b649a5e725d93c3cdfecb70ee49
willDrr <willxdd97@gmail.com>
2022-04-08 17:22:43 -0600

    Renaming email provider(temporal)

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: "from@example.com"
   # default from: "5tgrc.test@inbox.testmail.app"
-  default from: "wild3r@#{MAILGUN_DOMAIN_NAME}"
+  default from: "wild3r@email.com"
   layout "mailer"
 end
