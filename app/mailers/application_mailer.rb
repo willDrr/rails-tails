3ff2595b5ec6e88bbdcf3c2bc4df20e4650cf89e
willDrr <willxdd97@gmail.com>
2022-04-08 20:28:58 -0600

    running rubocop offenses

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -3,6 +3,6 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: "from@example.com"
   # default from: "5tgrc.test@inbox.testmail.app"
-  default from: "wild3r@email.com"
-  layout "mailer"
+  default from: 'wild3r@email.com'
+  layout 'mailer'
 end
