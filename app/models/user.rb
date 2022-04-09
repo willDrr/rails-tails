591e8089a71fb49664f1f8568d468ec0a95dbc7c
willDrr <willxdd97@gmail.com>
2022-04-09 14:29:33 -0600

    removing confirmable action from devise user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :omniauthable, omniauth_providers: %i[google_oauth2]
+         #:confirmable,
+         :omniauthable, omniauth_providers: %i[google_oauth2]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
---
+++ db/migrate/20220409201901_chnage_email_confirmation_constrain.rb
@@ -0,0 +1,7 @@
+class ChnageEmailConfirmationConstrain < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :users, :confirmation_token, true
+    change_column_null :users, :confirmed_at, true
+    change_column_null :users, :confirmation_sent_at, true
+  end
+end
