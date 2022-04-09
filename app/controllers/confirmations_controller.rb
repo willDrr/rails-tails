7ae5cf16d3f3d2f91c950a787a5b3f54f2e5559d
willDrr <willxdd97@gmail.com>
2022-04-09 13:16:48 -0600

    config mailgun

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -5,6 +5,7 @@ class ConfirmationsController < Devise::ConfirmationsController
 
   def after_confirmation_path_for(_resource_name, resource)
     sign_in(resource)
+    UserMailer.send_message(resource)
     home_index_path
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,25 @@
 # frozen_string_literal: true
 
+require 'mailgun-ruby'
+
 class UserMailer < ApplicationMailer
-  def send_email
+  def self.send_email(resource)
     api_key = ENV['MAILGUN_API_KEY']
     domain_name = ENV['MAILGUN_DOMAIN_NAME']
-    RestClient.post(
-      "https://api:#{api_key}@api.mailgun.net/v3/#{domain_name}/messages",
+    # RestClient.post(
+    #   "https://api:#{api_key}@api.mailgun.net/v3/#{domain_name}/messages",
+    #   from: "Excited user <mailgun@#{domain_name}>",
+    #   to: @user.email,
+    #   subject: 'Hello world'
+    # )
+    email_client = Mailgun::Client.new(api_key)
+
+    message_params = {
       from: "Excited user <mailgun@#{domain_name}>",
-      to: @user.email,
-      subject: 'Hello world'
-    )
+      to: resource.email,
+      suject: 'Email confirmation',
+      text: 'Please, to continue using our website, confirm your account'
+    }
+    email_client.send_message("mailgun@#{domain_name}", message_params)
   end
 end
