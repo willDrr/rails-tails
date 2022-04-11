7300e6eebe1ce3d2631ffe37458e76faaffe6ad6
willDrr <willxdd97@gmail.com>
2022-04-10 18:26:58 -0600

    protecting hotel route

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,5 +1,6 @@
 class HotelsController < ApplicationController
   before_action :set_hotel, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: %i[new edit destroy]
 
   # GET /hotels or /hotels.json
   def index
