4c65f76702b0bd2cede2de1cb54763a5d25d21f5
willDrr <willxdd97@gmail.com>
2022-07-09 13:17:28 -0600

    fixing typo

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: "mailgun@#{ENV['MAILGUN_DOMAIN_NAME']}", to: current_user.email
+  default from: "mailgun@#{ENV['MAILGUN_DOMAIN_NAME']}"
   layout 'mailer'
 end
